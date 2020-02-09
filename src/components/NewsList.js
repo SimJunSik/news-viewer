5761872d57cf36cb1c0af718a8743c719d75e89e
SimJunSik <wnstlr24@naver.com>
2020-02-09 19:12:33 +0900

    NewsList에서 useEffect 부분 usePromise 커스텀 Hook으로 리펙토링

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import NewsItem from './NewsItem';
 import axios from 'axios';
+import usePromise from '../lib/usePromise';
 
 const NewsListBlock = styled.div`
   box-sizing: border-box;
@@ -18,37 +19,29 @@ const NewsListBlock = styled.div`
 `;
 
 const NewsList = ({ category }) => {
-  const [articles, setArticles] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const query = category === 'all' ? '' : `&category=${category}`;
-        const response = await axios.get(
-          `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=29a011a8ce9c494092ce5d63584efb9e`,
-        );
-        setArticles(response.data.articles);
-      } catch (e) {
-        console.log(e);
-      }
-      setLoading(false);
-    };
-    fetchData();
+  const [loading, response, error] = usePromise(() => {
+    const query = category === 'all' ? '' : `&category=${category}`;
+    return axios.get(
+      `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=29a011a8ce9c494092ce5d63584efb9e`,
+    );
   }, [category]);
 
   // 대기 중일 때
   if (loading) {
     return <NewsListBlock>대기 중 ..</NewsListBlock>;
   }
-  // 아직 articles 값이 설정되지 않았을 때
-  // 처리해주지 않으면 artilces 가 null 일 때 렌더링 과정에서 오류
-  if (!articles) {
+  // 아직 response 값이 설정되지 않았을 때
+  // 처리해주지 않으면 response 가 null 일 때 렌더링 과정에서 오류
+  if (!response) {
     return null;
   }
 
+  if (error) {
+    return <NewsListBlock>에러 발생!</NewsListBlock>;
+  }
+
   // articles 값이 유효할 때
+  const { articles } = response.data;
   return (
     <NewsListBlock>
       {articles.map(article => (
---
+++ src/lib/usePromise.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react';
+
+// usePromise를 사용하면 NewsList에서 대기 중 상태 관리와 useEffect 설정을 직접 하지 않아도 되므로
+// 코드가 훨씬 간결해진다. 요청 상태를 관리할 때 무조건 커스텀 Hook을 만들어서 사용해야 하는건 아니지만
+// 상황에 따라 적절히 사용하면 좋은 코드를 만들 수 있음
+export default function usePromise(promiseCreator, deps) {
+  // 대기 중/완료/실패에 대한 상태 관리
+  const [loading, setLoading] = useState(false);
+  const [resolved, setResolved] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const process = async () => {
+      setLoading(true);
+      try {
+        const resolved = await promiseCreator();
+        setResolved(resolved);
+      } catch (e) {
+        setError(e);
+      }
+      setLoading(false);
+    };
+    process();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, deps);
+
+  return [loading, resolved, error];
+}
